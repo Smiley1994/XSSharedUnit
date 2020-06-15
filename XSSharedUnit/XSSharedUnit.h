25ea1a2a02e61426b6b0a0f1042d39f716a772df
songf <smileysun1994@gmail.com>
2020-06-15 16:35:08 +0800

    update faild file

--- XSSharedUnit/XSSharedUnit.h
+++ XSSharedUnit/XSSharedUnit.h
@@ -41,5 +41,3 @@ FOUNDATION_EXPORT const unsigned char XSSharedUnitVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <XSSharedUnit/PublicHeader.h>
 
-
-#import
