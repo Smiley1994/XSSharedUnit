38ae98f9bf8c28f434301683b6124a2881ef9465
songf <smileysun1994@gmail.com>
2020-06-15 15:29:08 +0800

    Initial Commit

---
+++ XSSharedUnit/XSSharedUnit.h
@@ -0,0 +1,19 @@
+//
+//  XSSharedUnit.h
+//  XSSharedUnit
+//
+//  Created by Good_Morning_ on 2020/6/15.
+//  Copyright © 2020 Good_Morning_. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for XSSharedUnit.
+FOUNDATION_EXPORT double XSSharedUnitVersionNumber;
+
+//! Project version string for XSSharedUnit.
+FOUNDATION_EXPORT const unsigned char XSSharedUnitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XSSharedUnit/PublicHeader.h>
+
+
